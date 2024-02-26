b804b38ca387ecfd1949794d8f14722b5d4fc042
snowmaze-dev <snowmaze@mail.ru>
2024-02-26 14:15:20 +0200

    Implement loadPagesUntil extension

--- pagingflowlibrary/src/commonMain/kotlin/ru/snowmaze/pagingflow/utils/PagingFlowUtils.kt
+++ pagingflowlibrary/src/commonMain/kotlin/ru/snowmaze/pagingflow/utils/PagingFlowUtils.kt
@@ -1,16 +1,19 @@
 package ru.snowmaze.pagingflow.utils
 
+import ru.snowmaze.pagingflow.PaginationDirection
 import ru.snowmaze.pagingflow.PagingFlow
 import ru.snowmaze.pagingflow.result.LoadNextPageResult
 
 suspend fun <Key : Any, Data : Any, PagingStatus : Any> PagingFlow<Key, Data, PagingStatus>.loadPagesUntil(
+    paginationDirection: PaginationDirection =
+        pagingFlowConfiguration.defaultParamsProvider().paginationDirection,
     maxLoadPages: Int? = null,
     shouldLoadNext: (LoadNextPageResult.Success<Key, Data>) -> Boolean,
 ) {
     var result: LoadNextPageResult<Key, Data>
     var count = 0
     do {
-        result = loadNextPageWithResult()
+        result = loadNextPageWithResult(paginationDirection)
         count++
     } while (result is LoadNextPageResult.Success<Key, Data> &&
         result.hasNext && shouldLoadNext(result)
