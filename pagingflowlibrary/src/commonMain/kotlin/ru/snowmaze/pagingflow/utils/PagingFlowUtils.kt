a09c7a8ea88511eeea9b3d1cdb38bf143eab7861
snowmaze-dev <snowmaze@mail.ru>
2024-02-26 14:00:00 +0200

    Implement loadPagesUntil extension

--- pagingflowlibrary/src/commonMain/kotlin/ru/snowmaze/pagingflow/utils/PagingFlowUtils.kt
+++ pagingflowlibrary/src/commonMain/kotlin/ru/snowmaze/pagingflow/utils/PagingFlowUtils.kt
@@ -4,12 +4,16 @@ import ru.snowmaze.pagingflow.PagingFlow
 import ru.snowmaze.pagingflow.result.LoadNextPageResult
 
 suspend fun <Key : Any, Data : Any, PagingStatus : Any> PagingFlow<Key, Data, PagingStatus>.loadPagesUntil(
+    maxLoadPages: Int? = null,
     shouldLoadNext: (LoadNextPageResult.Success<Key, Data>) -> Boolean,
 ) {
     var result: LoadNextPageResult<Key, Data>
+    var count = 0
     do {
         result = loadNextPageWithResult()
+        count++
     } while (result is LoadNextPageResult.Success<Key, Data> &&
         result.hasNext && shouldLoadNext(result)
+        && (maxLoadPages == null || maxLoadPages > count)
     )
 }
